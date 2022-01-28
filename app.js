47df2b70fa1190d71438326f9fad8c9cbb044dd9
Pramod Reddy Gonegari <s541903@nwmissouri.edu>
2022-01-28 16:07:54 -0600

    changed back base_url to callback_url

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var strategy = new Auth0Strategy(
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL:
-      process.env.BASE_URL || 'http://localhost:3000/callback'
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
   },
 
 
